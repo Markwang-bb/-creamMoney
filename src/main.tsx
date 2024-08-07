556180423b03b1e0b3a07b5e54967fe287c272ac
Markwang-bb <kissmyfacebaby11@gmail.com>
2024-08-08 03:49:12 +0800

    演示 unocss 的用法

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import './global.scss'
+import 'virtual:uno.css'
 
 const div = document.getElementById('root') as HTMLElement
 
