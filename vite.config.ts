fa5b15f5c4c9fcae6a4ab1b32279e283e8ada733
Markwang-bb <kissmyfacebaby11@gmail.com>
2024-08-08 13:47:09 +0800

    使用 vite-plugin-mock 在本地创建 mock 服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Unocss(),
-    react(),
-  ],
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+  return {
+    plugins: [
+      Unocss(),
+      react(),
+      viteMockServe({
+        mockEnabled: !isBuild // 根据 `isBuild` 配置 mockServe
+      })
+    ]
+  }
 })
-
