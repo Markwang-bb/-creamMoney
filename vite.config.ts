59c93a23abc997a52f21a4f5d6789cca875a2b7f
Markwang-bb <kissmyfacebaby11@gmail.com>
2024-08-08 18:57:04 +0800

    引入 svgsprites Vite 插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
 import { viteMockServe } from 'vite-plugin-mock'
+import { svgsprites } from './vite_plugins/svgsprites'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -15,7 +16,8 @@ export default defineConfig(({ command }) => {
       react(),
       viteMockServe({
         mockEnabled: !isBuild // 根据 `isBuild` 配置 mockServe
-      })
+      }),
+      svgsprites()
     ]
   }
 })
