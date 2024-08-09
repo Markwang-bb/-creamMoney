7cab442acb2fd45f3410346becb46c4dafe85f74
Markwang-bb <kissmyfacebaby11@gmail.com>
2024-08-09 16:26:06 +0800

    修改 Welcome 界面，即使页面被浏览器挡住，也不会影响交互

--- src/pages/Welcome2.tsx
+++ src/pages/Welcome2.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import p from '../assets/images/welcome2.svg'
 
 export const Welcome2: React.FC = () => {
@@ -7,7 +8,10 @@ export const Welcome2: React.FC = () => {
       <h2 text-32px mt-48px>
         每日提醒 <br />
         不会遗漏每一笔账单  
-      </h2>     
+      </h2>    
+      <div mt-64px>
+        <Link text-32px color="#6035BF" font-bold to="/welcome/3">下一页</Link>
+      </div>
     </div>
   )
 }
