a0da5c5c63c8d5b8c1d846fb3534a3429855be48
Markwang-bb <kissmyfacebaby11@gmail.com>
2024-08-12 06:12:25 +0800

    926064

--- src/pages/ItemsPageError.tsx
+++ src/pages/ItemsPageError.tsx
@@ -1,11 +1,10 @@
-import { Navigate, useRouteError } from 'react-router-dom'
-import { ErrorEmptyData, useLocation,ErrorUnauthorized } from '../errors'
+import { Navigate, useLocation, useRouteError } from 'react-router-dom'
+import { ErrorEmptyData, ErrorUnauthorized } from '../errors'
 
 export const ItemsPageError: React.FC = () => {
   const error = useRouteError()
   const e = error as Error
   const loc = useLocation()
-
   if (e instanceof ErrorUnauthorized) {
     const from = encodeURIComponent(`${loc.pathname}${loc.search}`)
     return <Navigate to={`/sign_in?from=${from}`} />
